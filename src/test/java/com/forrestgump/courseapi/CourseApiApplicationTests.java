482c05aaed284b644af590e214894b3b3f715d50
Christian Estevam <christianestevamb@gmail.com>
2025-01-27 23:40:37 -0300

    init: setup

---
+++ src/test/java/com/forrestgump/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forrestgump.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
